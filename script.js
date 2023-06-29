e558f3154d35191a5bc89173aed7d312727ca392
Dê Oliveira <105293266+deoliveira10@users.noreply.github.com>
2023-06-28 23:36:12 -0300

    Update script.js
    adicionando a contagem de projetos

---
+++ script.js
@@ -0,0 +1,4 @@
+window.addEventListener('DOMContentLoaded', function() {
+    var projectsCount = document.getElementsByClassName('project').length;
+    document.getElementById('projectsCount').textContent = projectsCount;
+  });
